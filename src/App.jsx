92f5a78134c1c67314cf116560423402383599c9
dumax1 <157002201+dumax1@users.noreply.github.com>
2024-02-15 08:34:08 +0000

    mesaj

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [contor, setContor]= useState(10);
+
+  const handleClick= () => {
+    setContor(contor + 2);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <h1>Valoarea contorului: {contor}</h1>
+    <button onClick={handleClick}>Apasa ma</button>
+
     </div>
-  );
+
+  )
+
 }
+
+   
 
 export default App;
